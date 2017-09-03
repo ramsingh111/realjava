7a369f44e4ee8af86ad5d7a41bbadcbc46898b4d
ramsingh111 <ramsinghtamang1234@gmail.com>
2017-09-03 13:58:35 +0545

    operator first step

---
+++ src/ramsingh/operatorDemo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramsingh;
+
+/**
+ *
+ * @author NCC
+ */
+public class operatorDemo {
+    
+     public static void main(String[] args){
+         int a = 12;
+         int b = 18;
+         System.out.println(a>b);
+         System.out.println(a > 11 || b < 15);
+         int c = 18;
+         int d = 27;
+        int max = a>b?a:b;
+         System.out.println();
+         
+     }
+    
+}
