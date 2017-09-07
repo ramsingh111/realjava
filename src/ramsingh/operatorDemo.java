9a6bb80ca0b82459b988311a242f6dcbe51f997e
ramsingh111 <ramsinghtamang1234@gmail.com>
2017-09-07 13:44:10 +0545

    while loop

--- src/ramsingh/operatorDemo.java
+++ src/ramsingh/operatorDemo.java
@@ -39,10 +39,67 @@ public class operatorDemo {
          System.out.println(gender=='M'?"Hello mr "+name:"Hello miss "+name);
          for (int j = 1 ;j<11  ;j++ ){
              for (int i = 1 ; i<11;i++){
-             System.out.print(j+" * "+i+" = "+i*j+"\t");   
+                System.out.print(j+" * "+i+" = "+i*j+"\t");   
+            }
+          System.out.println("");
+        }
+         String names[]={"rahul","sanjay","gita","ram"};
+         String nos[]={"1211323","232323","32434","232445"};
+         char genders[] = {'m','m','f','m'};
+         for(int i = 0;i<names.length;i++){
+             System.out.println("name is"+names[i]+"---"+(genders[i]== 'm'?"his":"her")+" phone number is " + nos[i]);
+             
+         
          }
-             System.out.println("");
+         
+         for (String n :names){
+             System.out.println("my name is" + n);
+         
+     } 
+          String searchTeam="nini";
+          boolean found=false;
+          for(String n:names){
+            if (n==searchTeam){
+                found= true;
+                break;
+                
+          }
+           
+         
+      }
+          System.out.println(found?"congratulation":"sorry"); 
+          
+          
+          int i = 0;
+           while (i<=100){
+              System.out.println(i);
+              i++;
+          }
+          System.out.println(getMax(50,98));
+          
+          
+           System.out.println(getSum(50,60));
+           
+           System.out.println(getSum(787,785,45545,4544,5454));
+           
      }
-
-}
-}
+     public static int getMax(int a , int b){
+        int max = a>b ? a:b;
+        return max;
+        
+        
+         
+   }
+     public static int getSum(int a , int b){
+         int sum = a+b;
+         return sum;
+         
+     }
+     public static int getSum(int... nums){
+        int sum=0;
+        for(int i:nums){
+        sum+=i;
+        }
+        return sum;
+     }
+}   
